046d6dac1d5088a0cf942c738c5c085bc4a0ad6e
Maria Kuznetsova <maria.kuznetsova@itechart-group.com>
2017-09-22 00:37:38 +0530

    update script

---
+++ ecoventAppStart.sh
@@ -0,0 +1,5 @@
+./hubserver.sh status
+./hubserver.sh start
+./clientserver.sh start
+./dbworker.sh start
+
--- profiling_script.sh
+++ profiling_script.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+if [ ! -d "report" ]; then 
+	mkdir report
+fi
+
+logfilename=report/$0_log_`date | awk '{print $1$2$3_$4}'`
+touch $logfilena
+
 pids=`ps -ef | grep -v root | awk '{print $2}'`
 processes=`ps -ef | grep -v root | awk '{print $8}'`
 
 for  epid in $pids
 do
         echo "files opened by $epid process"
+	processName=`cat /proc/$epid/cmdline | xargs -0 echo`
+	echo "analysing"  $processName $epid >> $logfilename 
         temp=`ls -ltr /proc/$epid/fd | grep -v socket | grep -v pipe | grep -v dev | grep -v anon | awk '{print $11}'`
+	echo "opened log files by this process" >> $logfilename
+ #	echo $temp >> $logfilename
 	files=$files.$temp
+	for etemp in $temp
+	do
+		du -h $etemp >> $logfilename
+	done
+	echo "---------------------------------------" >> $logfilename
+	echo '' >> $logfilename
 done
-> fileset
+
+if [ -f "fileset" ]; then 
+	> fileset
+fi 
+
 for efile in $files
 do
 	echo $efile  | sed 's/\.*$//g' >> fileset
@@ -19,14 +40,16 @@ cat fileset | sort -u > fileset1
 
 afiles=`cat fileset1`
 
-> vmoutput
+rm fileset fileset1
+
+if [ -f "vmoutput" ]; then 
+	> vmoutput
+fi
 
 kk=`date | awk '{print $4}'`
+
 for efile in $afiles
 do
 	echo "$efile footrpint in cache" >> '/home/ecoventstaff/tools/report/'$kk.'log'
 	/home/ecoventstaff/tools/vmtouch/vmtouch $efile >> '/home/ecoventstaff/tools/report/'$kk.'log'
 done
-
-#echo $files | sort | uniq
-
